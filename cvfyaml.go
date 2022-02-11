3b35c563aa86b5795aee055fd09cad14653a4383
Cedric VAN FRACHEM <cedric.vanfrachem.e@thalesdigital.io>
2022-02-11 17:20:39 +0100

    implemented children deletion rule

--- cvfyaml.go
+++ cvfyaml.go
@@ -9,61 +9,6 @@ import (
 	"log"
 )
 
-func Example() {
-	y := `---
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: sample-deployment
-spec:
-  template:
-    spec:
-      containers:
-      - name: nginx
-        image: nginx
-        ports:
-        - containerPort: 80
-      - name: nginy
-        image: nginy
-        ports:
-        - containerPort: 81
-`
-	var n yaml.Node
-
-	err := yaml.Unmarshal([]byte(y), &n)
-	if err != nil {
-		log.Fatalf("cannot unmarshal data: %v", err)
-	}
-
-	p, err := yamlpath.NewPath("$..spec.containers[*].image")
-	if err != nil {
-		log.Fatalf("cannot create path: %v", err)
-	}
-
-	q, err := p.Find(&n)
-	if err != nil {
-		log.Fatalf("unexpected error: %v", err)
-	}
-
-	for _, i := range q {
-		i.Value = "example.com/user/" + i.Value
-	}
-
-	var buf bytes.Buffer
-	e := yaml.NewEncoder(&buf)
-	defer e.Close()
-	e.SetIndent(2)
-
-	err = e.Encode(&n)
-	if err != nil {
-		log.Printf("Error: cannot marshal node: %v", err)
-		return
-	}
-
-	fmt.Println(buf.String())
-
-}
-
 func readInput() yaml.Node {
 	yamlFile, err := ioutil.ReadFile("mycrd.yaml")
 
@@ -87,8 +32,9 @@ type SetSpec struct {
 }
 
 type RuleSpec struct {
-	Match string
-	Set   []SetSpec
+	Match                   string
+	Set                     []SetSpec
+	DeleteChildrenThatMatch string `yaml:"deleteChildrenThatMatch"`
 }
 
 func readRules() map[string]RuleSpec {
@@ -119,11 +65,45 @@ func applyRule(ruleSpec RuleSpec, document yaml.Node) {
 	}
 
 	for _, matchingNode := range q {
-		for _, setSpec := range ruleSpec.Set {
-			applySet(setSpec, matchingNode)
+		if ruleSpec.Set != nil {
+			for _, setSpec := range ruleSpec.Set {
+				applySet(setSpec, matchingNode)
+			}
+		} else {
+			deleteChildrenThatMatch(ruleSpec.DeleteChildrenThatMatch, matchingNode)
 		}
-
 	}
+}
+
+func deleteChildrenThatMatch(match string, parentNode *yaml.Node) {
+	childrenMatchString := "$[" + match + "]"
+	p, err := yamlpath.NewPath(childrenMatchString)
+	if err != nil {
+		log.Fatalf("cannot create children match lookup: %v", err)
+	}
+
+	nodesToRemove, err := p.Find(parentNode)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+	nodesToKeep := []*yaml.Node{}
+
+	for _, childNode := range parentNode.Content {
+		if !nodesInList(childNode, nodesToRemove) {
+			nodesToKeep = append(nodesToKeep, childNode)
+		}
+	}
+
+	parentNode.Content = nodesToKeep
+}
+
+func nodesInList(node *yaml.Node, nodesList []*yaml.Node) bool {
+	for _, item := range nodesList {
+		if item == node {
+			return true
+		}
+	}
+	return false
 }
 
 func buildStringNodes(key string, value string) []*yaml.Node {
@@ -179,7 +159,6 @@ func applySet(spec SetSpec, node *yaml.Node) {
 				(*r).Value = fieldValue.Value
 				(*r).Content = fieldValue.Content
 				(*r).Column = fieldValue.Column
-				log.Printf("hello\n")
 			} else {
 				matchingNode.Content = append(matchingNode.Content, buildKeyNode(fieldName), &fieldValue)
 			}
@@ -204,7 +183,8 @@ func outputResult(doc yaml.Node) {
 func main() {
 	rules := readRules()
 	document := readInput()
-	for _, ruleSpec := range rules {
+	for ruleName, ruleSpec := range rules {
+		log.Printf("Applying rule '%v'...\n", ruleName)
 		applyRule(ruleSpec, document)
 	}
 	outputResult(document)
