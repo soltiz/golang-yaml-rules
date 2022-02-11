1f01ef251789baec01595c9689a48b98f8bcafe2
Cedric VAN FRACHEM <cedric.vanfrachem.e@thalesdigital.io>
2022-02-11 15:04:00 +0100

    working set on strings

--- cvfyaml.go
+++ cvfyaml.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/vmware-labs/yaml-jsonpath/pkg/yamlpath"
 	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"log"
 )
 
@@ -63,7 +64,136 @@ spec:
 
 }
 
+func readInput() yaml.Node {
+	yamlFile, err := ioutil.ReadFile("mycrd.yaml")
+
+	if err != nil {
+		log.Fatal("Unable to read input file", err)
+	}
+
+	var rootNode yaml.Node
+
+	err = yaml.Unmarshal([]byte(yamlFile), &rootNode)
+	if err != nil {
+		log.Fatalf("cannot unmarshal data: %v", err)
+	}
+
+	return rootNode
+}
+
+type SetSpec struct {
+	Subpath string
+	Values  map[string]string
+}
+
+type RuleSpec struct {
+	Match string
+	Set   []SetSpec
+}
+
+func readRules() map[string]RuleSpec {
+	yamlFile, err := ioutil.ReadFile("rules.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make(map[string]RuleSpec)
+	err2 := yaml.Unmarshal(yamlFile, &data)
+
+	if err2 != nil {
+		log.Fatal("Unable to unmarshall rules yaml file", err2)
+	}
+	return data
+}
+
+func applyRule(ruleSpec RuleSpec, document yaml.Node) {
+	p, err := yamlpath.NewPath(ruleSpec.Match)
+	if err != nil {
+		log.Fatalf("cannot create path: %v", err)
+	}
+
+	q, err := p.Find(&document)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	for _, matchingNode := range q {
+		for _, setSpec := range ruleSpec.Set {
+			applySet(setSpec, matchingNode)
+		}
+
+	}
+}
+
+func buildStringNodes(key string, value string) []*yaml.Node {
+	keyNode := &yaml.Node{
+		Kind:  yaml.ScalarNode,
+		Tag:   "!!str",
+		Value: key,
+	}
+	valueNode := &yaml.Node{
+		Kind:  yaml.ScalarNode,
+		Tag:   "!!str",
+		Value: value,
+	}
+	return []*yaml.Node{keyNode, valueNode}
+}
+
+func applySet(spec SetSpec, node *yaml.Node) {
+
+	p, err := yamlpath.NewPath(spec.Subpath)
+	if err != nil {
+		log.Fatalf("cannot create subpath: %v", err)
+	}
+
+	matches, err := p.Find(node)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	for _, matchingNode := range matches {
+		for fieldName, fieldValue := range spec.Values {
+
+			fieldPath, err := yamlpath.NewPath("$." + fieldName)
+			if err != nil {
+				log.Fatalf("cannot create field subpath: %v", err)
+			}
+
+			fieldMatches, err := fieldPath.Find(matchingNode)
+
+			if len(fieldMatches) != 0 {
+				fieldMatches[0].Value = fieldValue
+			} else {
+				matchingNode.Content = append(matchingNode.Content, buildStringNodes(fieldName, fieldValue)...)
+
+			}
+			for _, fieldNode := range fieldMatches {
+				fieldNode.Value = fieldValue
+			}
+		}
+	}
+}
+
+func outputResult(doc yaml.Node) {
+	var buf bytes.Buffer
+	e := yaml.NewEncoder(&buf)
+	defer e.Close()
+	e.SetIndent(2)
+
+	err := e.Encode(&doc)
+	if err != nil {
+		log.Printf("Error: cannot marshal node: %v", err)
+		return
+	}
+	fmt.Println(buf.String())
+}
+
 func main() {
-	Example()
-	log.Fatalf("Unable to read client secret file")
+	rules := readRules()
+	document := readInput()
+	for _, ruleSpec := range rules {
+		applyRule(ruleSpec, document)
+	}
+	outputResult(document)
 }
